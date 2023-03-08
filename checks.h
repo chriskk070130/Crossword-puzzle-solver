28254660922e55333e04b63612fb2442af49da7b
christoskotsis <79981141+chriskk070130@users.noreply.github.com>
2023-03-08 21:40:57 +0200

    .

--- checks.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-#include "structs.h"
-int check_solution(FILE* dictionary, char** crossw, int dimensions, Slot* slots_table, int n_myslots);
-int check_if_completed(Slot* slots_table, int n_myslots);
